cc7f53e0c99337801809ce2c0b6994967eb92fc5
ALWINgm <150226233+ALWINgm@users.noreply.github.com>
2024-05-22 01:56:58 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// if(5<10){
+    // console.log("5 is less")
+// }
+// else {
+    // console.log ("10 is greater")
+// }
+// let age=10
+// if(age>18){
+    // console.log("you are eligible for driving licence")
+// }
+// else{
+// console.log("you are not eligible for driving licence")
+// }
+// let time=19;
+// if(time<=12){
+// console.log("GOOD MORNING")
+// }
+// else if (time>12 && time <=15){
+//     console.log("Good AferNoon")
+// }
+// else if(time>15 && time<=19){
+//     console.log("Good Evening")
+// }
+// else {
+//     console.log(" Good Night")
+// }
+// let a= 18;
+// (a<20)?console.log("a is less than 20"):console.log("a is greater than 20")
+// for(let i=0;i<=10;i++){
+// console.log(i)
+// }
+// let fruits =["apple","orange","grapes","banana"];
+// for(let i=0; i<fruits.length;i++){
+// console.log (fruits[i]);
+// }
+// [let num1,2,3,4,5,6];
+// if (i=0; i<num.length;i++){
+//     if (num[i]%2==0){
+//         console.log(`${num[i]} is even`)
+//     }
+//     else{
+//         console.log(`${num[i]} is odd`)
+//     }
+// }
+// FIND the sum of array
+// let values =[1,2,3,4,5,6,7,8,9]
+// let sum=0 
+// for(let i=0; i<values.length;i++){
+//     sum+=values[i];
+// } values=[1,2,3,4,5]
+// console.log("sum is "+ sum)
+// Javascript program to print
+// table of a number
+// Javascript program to print
+// table of a number
+
+// 
+StringoriginalStr = "Hello";
+StringreversedStr = "";
+
+for (int i = 0; i < originalStr.length(); i++) {
+  reversedStr = originalStr.charAt(i) + reversedStr;
+}
+
+System.out.println("Reversed string: "+ reversedStr);
+
